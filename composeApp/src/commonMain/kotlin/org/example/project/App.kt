7458d78aaeb3e6d5aa58a4b913869fd5004a5760
中川一樹 <137146752+sunshine-724@users.noreply.github.com>
2025-06-23 00:46:00 +0900

    マウスが置いたところに青色のペンがつく

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,6 +1,7 @@
 package org.example.project
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -9,26 +10,35 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import org.example.project.AppTheme
-
 
 // アプリ起動時に一度だけプラットフォーム固有の実装を取得する
 private val platform = getPlatform()
 
 @Composable
 fun App() {
+    //remember修飾子:再描画した際、かかっているデータ構造が初期化されない(逆に言うとつけてないと初期化される)
+    val circles = remember { mutableStateListOf<Offset>() }
+
     AppTheme {
-        // TextFieldの入力値を保持するためのState
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            drawLine(
-                color = Color.Red,
-                start = androidx.compose.ui.geometry.Offset(0f, 0f),
-                end = androidx.compose.ui.geometry.Offset(size.width, size.height),
-                strokeWidth = 5f
-            )
+        //pointerInput (マウスや指)の入力を受け取るためのModifier
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        circles.add(change.position)
+                    }
+                }
+        ) {
+            //Canvasの再描画が起きるたび、発火する
+            circles.forEach { position ->
+                drawCircle(color = Color.Blue, radius = 20f, center = position)
+            }
         }
     }
 }
